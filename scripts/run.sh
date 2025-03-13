301594061b4b4f9448b6b4d886f650d40f3eb351
Felix Möller <felix.moeller@helmholtz-berlin.de>
2025-03-13 23:37:09 +0100

    Update wrapper scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-# take two input arguments, config file and the GPU id
+# take input arguments, path to config file first, then GPU id, then all other (optional) parameters
+GPU_ID=$1
+shift
 CONFIG_FILE=$1
-GPU_ID=$2
+shift
+ARGUMENTS=$@
 WD=$(realpath $(dirname "$0"))
 
 # Check for set config and GPU id
 if [ "$#" -ne 2 ]; then
-    echo "$(basename """$1""") CONFIG_FILE GPU_ID" >&2
-    exit 1
+	echo "$(basename """$1""") CONFIG_FILE GPU_ID" >&2
+	exit 1
 fi
 
 # create log file from the config file
@@ -20,7 +23,11 @@ LOG_FILE="logs/${CONFIG_FILE:8:-5}.txt"
 mkdir -p $(dirname $LOG_FILE)
 touch $LOG_FILE
 
+echo "Using additional parameters:" &> "$LOG_FILE"
+echo "  $ARGUMENTS" &>> "$LOG_FILE"
+echo &>> "$LOG_FILE"
+
 # Run the script
-python "$WD/../main.py" --config $CONFIG_FILE --device cuda:$GPU_ID &> $LOG_FILE
-python "$WD/../test.py" --config $CONFIG_FILE --device cuda:$GPU_ID &>> $LOG_FILE
-python "$WD/../plot.py" --config $CONFIG_FILE --device cuda:$GPU_ID
+python "$WD/../main.py" $ARGUMENTS --config $CONFIG_FILE --device cuda:$GPU_ID &>> $LOG_FILE
+python "$WD/../test.py" $ARGUMENTS --config $CONFIG_FILE --device cuda:$GPU_ID &>> $LOG_FILE
+python "$WD/../plot.py" $ARGUMENTS --config $CONFIG_FILE --device cuda:$GPU_ID
--- scripts/slurm-run.sh
+++ scripts/slurm-run.sh
@@ -2,30 +2,28 @@
 # SLURM wrapper for running experiments on JUWELS BOOSTER
 #
 # Intended to be run as:
-# $ srun -A hai_XXXX --ntasks 4 --gpus-per-task=4 ~/einsearch/scripts/slurm-run.sh configs/einspace/evolution_test/addnist/evolution.yaml
+# $ srun -A hai_XXXX --ntasks 4 --gpus-per-task=4 scripts/slurm-run.sh configs/einspace.lst 4
 set -e
 
 # Configuration variables
-CONFIG_FILE="$1"
+BATCH_FILE="$1"
+GPU_COUNT="$2"
 WD=$(realpath $(dirname "$0"))
 VENV_DIR="$WD/venv"
 SCRIPT_PATH="$WD/run.sh"
-GPU_COUNT=4
 
 # Check for set config
-if [ "$#" -ne 1 ]; then
-    echo "$(basename """$0""") CONFIG_FILE" >&2
-    exit 1
+if [ "$#" -ne 2 ]; then
+	echo "$(basename """$0""") BATCH_FILE GPU_COUNT" >&2
+	exit 1
 fi
 
 # Activate virtual environment
 if [ ! -d "$VENV_DIR" ]; then
-    echo "venv not found :("
-    exit 1
+	echo "venv not found :("
+	exit 1
 fi
 . "$VENV_DIR/bin/activate"
 
-for gpu_id in $(seq 0 "$((GPU_COUNT - 1))"); do
-    "$SCRIPT_PATH" "$CONFIG_FILE" "$gpu_id" &
-done
-wait
+# Parallelize via GNU Parallel
+paste -d ' ' "$BATCH_FILE" | parallel --jobs "$GPU_COUNT" --colsep ';' "$SCRIPT_PATH" "{1}" "{%}" {2}
