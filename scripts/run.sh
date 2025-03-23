5cef5d7fe235f11faf01d9bee589350cf1b3ae2e
Felix Möller <felix.moeller@helmholtz-berlin.de>
2025-03-23 22:12:14 +0100

    Fix scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -16,18 +16,21 @@ JOB_ID=$1
 shift
 ARGUMENTS=$@
 WD=$(realpath $(dirname "$0"))
+CONFIG_NAME="""$(basename "$CONFIG_FILE" .yaml)"""
 
 # Activate virtual environment
-WD=$(realpath $(dirname "$0"))
-VENV_DIR="$WD/venv"
-if [ ! -d "$VENV_DIR" ]; then
-	echo "venv not found. Creating one..."
-	python -m venv venv
-	. "$VENV_DIR/bin/activate"
-	pip install --no-input einops graphviz positional_encodings psutil pydot pygraphviz pympler rich termcolor tqdm typing_extensions
-else
-	. "$VENV_DIR/bin/activate"
+VENV_DIR="$WD/../venv"
+VENV_FILE="$VENV_DIR/bin/activate"
+if [ ! -f "$VENV_FILE" ]; then
+	echo "venv not found. Please create one using:"
+	echo python -m venv venv
+	echo source "$VENV_DIR/bin/activate"
+	echo pip install --no-input einops graphviz positional_encodings psutil pydot pygraphviz pympler rich termcolor tqdm typing_extensions
+	exit 1
 fi
+echo "Found venv at: $VENV_DIR"
+echo "Activating..."
+source "$VENV_FILE"
 
 # Limit PyTorch to single GPU
 export CUDA_VISIBLE_DEVICES="$((GPU_ID - 1))"
@@ -38,7 +41,7 @@ export CUBLAS_WORKSPACE_CONFIG=:4096:8
 
 # Create log file from the config file
 # e.g. configs/a/b/c/d.yaml -> logs/a/b/c/d.txt
-LOG_FILE="$WD/../logs/${CONFIG_FILE:8:-5}_${JOB_ID}_${GPU_ID}.txt"
+LOG_FILE="$WD/../logs/einsearch/${CONFIG_NAME}_${JOB_ID}_${GPU_ID}.txt"
 
 # Create log dir & file
 mkdir -p """$(dirname "$LOG_FILE")"""
@@ -55,6 +58,7 @@ echo "  $ARGUMENTS" &>> "$LOG_FILE"
 echo &>> "$LOG_FILE"
 
 # Run the script
+# python "$WD/../download.py" $ARGUMENTS --config "$CONFIG_FILE" &>> "$LOG_FILE"
 python "$WD/../main.py" $ARGUMENTS --config "$CONFIG_FILE" &>> "$LOG_FILE"
 python "$WD/../test.py" $ARGUMENTS --config "$CONFIG_FILE" &>> "$LOG_FILE"
 # python "$WD/../plot.py" $ARGUMENTS --config "$CONFIG_FILE"
--- scripts/slurm-run.sh
+++ scripts/slurm-run.sh
@@ -2,7 +2,11 @@
 # SLURM wrapper for running experiments on JUWELS BOOSTER
 #
 # Intended to be run as:
-# $ sbatch -D ./scripts -A hai_XXXX --time 24:00:00 --ntasks 1 --gpus-per-task 4 ./scripts/slurm-run.sh ./configs/einspace.lst 4
+# $ sbatch -D /einsearch --export ALL -A hai_XXXX --time 24:00:00 --ntasks 1 --gpus-per-task 4 /einsearch/scripts/slurm-run.sh /einsearch/configs/einspace.lst 4
+#
+# Set custom log files for SLURM
+#SBATCH --output=logs/slurm/slurm-%j.out
+#SBATCH --error=logs/slurm/slurm-%j.err
 set -e
 
 # Check for set config
@@ -14,17 +18,13 @@ fi
 # Configuration variables
 BATCH_FILE="$1"
 GPU_COUNT="$2"
-WD="$PWD"
-SCRIPT_PATH="$WD/run.sh"
-
-# Set custom log files for SLURM
-#SBATCH --output=${WD}/../logs/slurm/slurm-%j.out
-#SBATCH --error=${WD}/../logs/slurm/slurm-%j.err
+REPO_ROOT="$PWD"
+SCRIPT_PATH="$REPO_ROOT/scripts/run.sh"
 
 # Load JUWELS modules
 while read -r mod; do
 	module load "$mod"
-done < "$WD/juwels_modules.txt"
+done < "$REPO_ROOT/scripts/juwels_modules.txt"
 
 # Parallelize via GNU Parallel
-paste -d ' ' "$BATCH_FILE" | parallel --jobs "$GPU_COUNT" --colsep ';' "$SCRIPT_PATH" "../{1}" "{%}" "$SLURM_JOB_ID" {2}
+paste -d ' ' "$BATCH_FILE" | parallel --jobs "$GPU_COUNT" --colsep ';' "$SCRIPT_PATH" "{1}" "{%}" "$SLURM_JOB_ID" {2}
