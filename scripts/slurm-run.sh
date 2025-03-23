d1de9d7f6cee645ded35b2ab9293352281f8d4c7
Felix Möller <felix.moeller@helmholtz-berlin.de>
2025-03-23 05:03:36 +0100

    Overhaul SLURM wrapper

--- scripts/slurm-run.sh
+++ scripts/slurm-run.sh
@@ -2,15 +2,8 @@
 # SLURM wrapper for running experiments on JUWELS BOOSTER
 #
 # Intended to be run as:
-# $ srun -A hai_XXXX --ntasks 4 --gpus-per-task=4 scripts/slurm-run.sh configs/einspace.lst 4
+# $ sbatch -D ./scripts -A hai_XXXX --time 24:00:00 --ntasks 1 --gpus-per-task 4 ./scripts/slurm-run.sh ./configs/einspace.lst 4
 set -e
-
-# Configuration variables
-BATCH_FILE="$1"
-GPU_COUNT="$2"
-WD=$(realpath $(dirname "$0"))
-VENV_DIR="$WD/venv"
-SCRIPT_PATH="$WD/run.sh"
 
 # Check for set config
 if [ "$#" -ne 2 ]; then
@@ -18,12 +11,20 @@ if [ "$#" -ne 2 ]; then
 	exit 1
 fi
 
-# Activate virtual environment
-if [ ! -d "$VENV_DIR" ]; then
-	echo "venv not found :("
-	exit 1
-fi
-. "$VENV_DIR/bin/activate"
+# Configuration variables
+BATCH_FILE="$1"
+GPU_COUNT="$2"
+WD="$PWD"
+SCRIPT_PATH="$WD/run.sh"
+
+# Set custom log files for SLURM
+#SBATCH --output=${WD}/../logs/slurm/slurm-%j.out
+#SBATCH --error=${WD}/../logs/slurm/slurm-%j.err
+
+# Load JUWELS modules
+while read -r mod; do
+	module load "$mod"
+done < "$WD/juwels_modules.txt"
 
 # Parallelize via GNU Parallel
-paste -d ' ' "$BATCH_FILE" | parallel --jobs "$GPU_COUNT" --colsep ';' "$SCRIPT_PATH" "{1}" "{%}" {2}
+paste -d ' ' "$BATCH_FILE" | parallel --jobs "$GPU_COUNT" --colsep ';' "$SCRIPT_PATH" "{1}" "{%}" "$SLURM_JOB_ID" {2}
