cab2e45ec81255436634cab22a874cdf8b645ace
Adri Gómez <107184677+AdriGoMar@users.noreply.github.com>
2025-09-02 13:35:07 +0200

    rename constrained_smith_waterman_crossover to recursive_constrained_smith_waterman_crossover

--- search_strategies/utils/recursive_constrained_smith_waterman.py
+++ search_strategies/utils/recursive_constrained_smith_waterman.py
@@ -1460,7 +1460,7 @@ def select_operations(operations, skewness = 0):
     return [operations[i] for i, v in enumerate(selected) if v == "1"]
 
 
-def constrained_smith_waterman_crossover(parent1, parent2):
+def recursive_constrained_smith_waterman_crossover(parent1, parent2):
     # build alignment matrix
     matrix = AlignmentMatrixRecursive(parent1, parent2, verbose=False)
     operations = matrix.nontrivial_ops
